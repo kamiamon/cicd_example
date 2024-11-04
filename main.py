316270775704c05ee3407f27d20a132759ab3221
kamiamon <121360693+kamiamon@users.noreply.github.com>
2024-11-04 13:52:54 +0300

    Fix bug at main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"msg": "Hello World!}
+    return {"msg": "Hello World!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
