aaa20e3a1291ed991865cca9d8e40642eb32f92c
kamiamon <121360693+kamiamon@users.noreply.github.com>
2024-11-04 13:51:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import uvicorn
-import os
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"msg": "Hello World!"}
-
-@app.get("/read-file")
-def read_file(path: str = Query(..., description="Path to the file to read")):
-    try:
-        with open(path, "r") as file:
-            content = file.read()
-        return {"content": content}
-    except Exception as e:
-        return {"error": str(e)}
+    return {"msg": "Hello World!}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
